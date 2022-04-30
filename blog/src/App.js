4d110e9ec07a264d26e9789ccd7a5c565d423800
Sage-Oh <7838qq@gmail.com>
2022-04-30 21:29:30 +0900

    state 활용

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [제목, a] = useState(['진주  상대동 카페 이로움', '맛집추천', '리액트 독학']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>세이지의 개발 블로그</div>
+      </div>
+      <div className='list'>
+        <h4> { 제목[0] } </h4>
+        <p>4월 29일 발행</p>
+      </div>
+      <div className='list'>
+        <h4> { 제목[1] } </h4>
+        <p>4월 29일 발행</p>
+      </div>
+      <div className='list'>
+        <h4> { 제목[2] } </h4>
+        <p>4월 29일 발행</p>
+      </div>
+
     </div>
   );
 }
